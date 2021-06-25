07fed3f9ce4ed6fb371ad521c87a286dfecdbf72
Andrey Vasilev <ao.vasilev@gmail.com>
2021-06-25 11:16:53 +0300

    Update for Sublime 4

--- Fastlane Runner/Interactor/ExternalApplications.swift
+++ Fastlane Runner/Interactor/ExternalApplications.swift
@@ -10,6 +10,10 @@ import Foundation
 import AppKit
 
 struct ExteranlApplications {
-    static var sublime: URL? { NSWorkspace.shared.urlForApplication(withBundleIdentifier: "com.sublimetext.3") }
+    static var sublime: URL? {
+        return ["com.sublimetext.4", "com.sublimetext.3"]
+            .compactMap { NSWorkspace.shared.urlForApplication(withBundleIdentifier: $0) }
+            .first
+    }
     static var textEdit: URL? { NSWorkspace.shared.urlForApplication(withBundleIdentifier: "com.apple.TextEdit") }
 }
